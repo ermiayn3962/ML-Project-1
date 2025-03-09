82a2de88dd19e0030e358f9c111ad9aedb4170f5
TomZhou145 <ztom300@gmail.com>
2025-03-09 18:57:08 -0400

    started cv

--- problem1.py
+++ problem1.py
@@ -5,6 +5,10 @@ import textwrap
 from sklearn.feature_extraction.text import CountVectorizer
 import sklearn.linear_model
 import sklearn.metrics
+import sklearn
+from sklearn.model_selection import train_test_split, KFold
+from sklearn import preprocessing
+from scipy.stats import trimboth
 
 if __name__ == '__main__':
     data_dir = 'data_readinglevel'
@@ -17,58 +21,75 @@ if __name__ == '__main__':
 
     # Print out 8 random entries
     tr_text_list = x_train_df['text'].values.tolist()
-    prng = np.random.RandomState(101)
-    rows = prng.permutation(np.arange(y_train_df.shape[0]))
+    y_rawData_list = np.array(y_train_df['Coarse Label'].values.tolist())
+
+    # print(type(y_rawData_list))
+
+
+    y_rawData_N = (y_rawData_list == "Key Stage 4-5").astype(int)
+ 
     
+    print(y_rawData_N)
     
-    # ##print
-    # for row_id in rows[:]:
-    #     text = tr_text_list[row_id]
-    #     print("row %5d | %s BY %s | y = %s" % (
-    #         row_id,
-    #         y_train_df['title'].values[row_id],
-    #         y_train_df['author'].values[row_id],
-    #         y_train_df['Coarse Label'].values[row_id],
-    #         ))
-    #     # Pretty print text via textwrap library
-    #     line_list = textwrap.wrap(tr_text_list[row_id],
-    #         width=70,
-    #         initial_indent='  ',
-    #         subsequent_indent='  ')
-    #     print('\n'.join(line_list))
-    #     print("")
-    # tr_text_list = ["I dont know what i am doing know don't",
-                #   "Hi this is just easy for don't know doing hi"
-                #   ] 
-    
-    vectorizer = CountVectorizer(lowercase=True, analyzer='word')
+    # TODO: Figure out what to store
+    vectorizer = CountVectorizer(lowercase=True, analyzer='word', token_pattern=r'[a-zA-Z]*')
     coords_occur_list = vectorizer.fit_transform(tr_text_list)
-
-    
     word_list = vectorizer.get_feature_names_out(tr_text_list)
-    # for x in word_list[:]:
-    #     index_of_word = vectorizer.vocabulary_.get(x)
-    #     print("occurance of", x ," ", occurance_of_word)
-    
+    x_rawData_NF = vectorizer.fit_transform(tr_text_list).toarray()
 
-    DICTIONARY = dict() 
+    x_train_df = trimboth(x_train_df, 0.25)
+    y_train_df = trimboth(y_train_df, 0.25)
     
+    
+    N, F = x_rawData_NF.shape
+    N1, = y_rawData_N.shape
 
-    tok_list = vectorizer.get_feature_names_out(tr_text_list)
-    for tok in tok_list:
-        if tok in DICTIONARY:
-            DICTIONARY[tok] += 1
-        else:
-            DICTIONARY[tok] = 1
+    assert(N == N1)
+
+    # Preprocessing the data
+    scaler = preprocessing.StandardScaler().fit(x_rawData_NF)
+    x_rawData_NF = scaler.transform(x_rawData_NF)
+
+    # print(x_rawData_NF.shape)
+    # print(y_rawData_list.shape)
     
-    
-    sorted_tokens = list(sorted(DICTIONARY, key=DICTIONARY.get, reverse=True))
-    for w in sorted_tokens[:10]:
-        print("%5d %s" % (DICTIONARY[w], w))
-    # print(word_list)
 
     # print(coords_occur_list.toarray())
-    print("end of operation")
+    
+    clf = sklearn.linear_model.LogisticRegression(penalty='l2',C=1.0, max_iter=5000, solver='lbfgs')
+    print("N", N)
+    print("F", F)
+
+    x_tr, x_te, y_tr, y_te = train_test_split(x_rawData_NF, y_rawData_N, test_size=0.2, random_state=69)
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=69, )
+
+    model_per_fold = list() 
+    tr_err_list = list()
+    te_err_list = list()
+    
+    for train_idx, test_idx in kf.split(x_tr):
+        x_train_split, x_test_split = x_tr[train_idx], x_tr[test_idx]
+        y_train_split, y_test_split = y_tr[train_idx], y_tr[test_idx]
+        
+        model_per_fold.append(clf.fit(x_train_split, y_train_split))
+        tr_err_list.append(1 - clf.score(x_train_split, y_train_split))
+        te_err_list.append(1 - clf.score(x_test_split, y_test_split))
+        
+
+
+
+    print("Training Error", tr_err_list)
+    print("Test Error", te_err_list)
+
+  
+
+    ##cross validae 6 folds/ 4 train 2 /test/
+
+
+    
+    
+
     
 
 
